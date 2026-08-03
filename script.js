fe194b3ed07a0af4b45a9b1da64de41a75a65f95
vish-2219 <135820808+vish-2219@users.noreply.github.com>
2026-08-03 21:04:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,96 +2,38 @@
 // CATEGORY TABS
 // ===============================
 
-const tabs = document.querySelectorAll(".tab");
+// CATEGORY TABS
 
+const tabs = document.querySelectorAll(".tab");
 const productGroups = document.querySelectorAll(".products");
 
-tabs.forEach(tab=>{
+tabs.forEach(tab => {
 
-    tab.addEventListener("click",()=>{
+    tab.addEventListener("click", () => {
 
-        tabs.forEach(t=>t.classList.remove("active"));
+        tabs.forEach(t => t.classList.remove("active"));
 
         tab.classList.add("active");
 
-        productGroups.forEach(group=>{
-
+        productGroups.forEach(group => {
             group.classList.remove("activeProducts");
-
         });
 
-        document
-        .getElementById(tab.dataset.category)
-        .classList.add("activeProducts");
+        document.getElementById(tab.dataset.category)
+            .classList.add("activeProducts");
 
     });
 
 });
 
-// ===============================
-// POPUP
-// ===============================
+// PRODUCT CLICK
 
-const popup = document.getElementById("arPopup");
+document.querySelectorAll(".product").forEach(product => {
 
-const popupTitle = document.getElementById("popupTitle");
+    product.addEventListener("click", () => {
 
-const viewer = document.getElementById("hiddenViewer");
-
-let selectedModel = "";
-
-let selectedPoster = "";
-
-document.querySelectorAll(".product").forEach(product=>{
-
-    product.addEventListener("click",()=>{
-
-        popup.style.display="flex";
-
-        popupTitle.innerHTML=product.dataset.name;
-
-        selectedModel=product.dataset.model;
-
-        selectedPoster=product.dataset.poster;
+        console.log(product.dataset.name);
 
     });
-
-});
-
-// ===============================
-// VIEW IN AR
-// ===============================
-
-document.getElementById("viewAR").addEventListener("click",()=>{
-
-    viewer.src=selectedModel;
-
-    viewer.poster=selectedPoster;
-
-    setTimeout(()=>{
-
-        viewer.activateAR();
-
-    },500);
-
-});
-
-// ===============================
-// CLOSE
-// ===============================
-
-document.getElementById("closePopup").addEventListener("click",()=>{
-
-    popup.style.display="none";
-
-});
-
-window.addEventListener("click",(e)=>{
-
-    if(e.target===popup){
-
-        popup.style.display="none";
-
-    }
 
 });
